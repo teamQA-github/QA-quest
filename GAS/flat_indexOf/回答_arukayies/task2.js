7159358245842f43300d6b63153275b83e4bacb5
arukayies <arukayies@gmail.com>
2020-10-28 20:51:32 +0900

    flatを使うように対応

--- GAS/flat_indexOf/回答_arukayies/task2.js
+++ GAS/flat_indexOf/回答_arukayies/task2.js
@@ -3,8 +3,7 @@ function getPassengerIdRow(num) {
     const passengerIds = SHEET.getRange("A:A").getValues();
 
     // 多次元配列をフラットにする
-    // https://qiita.com/yarnaimo/items/cd663e46a1676678396d
-    const flattenPassengerIds = [].concat(...passengerIds);
+    const flattenPassengerIds = passengerIds.flat();
 
     // 指定PassengerIdの行番号を返す
     return flattenPassengerIds.indexOf(num);
