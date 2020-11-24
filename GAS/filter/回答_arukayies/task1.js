961ffbf296e3a4ee431fc5390ed48c92abadc12d
arukayies <arukayies@gmail.com>
2020-11-24 21:12:07 +0900

    getValues()に直接flat()を繋げる

--- GAS/filter/回答_arukayies/task1.js
+++ GAS/filter/回答_arukayies/task1.js
@@ -21,14 +21,11 @@ function getCabinList(){
 
 // 最初にflatを活用する版1
 function getCabinListByFlat1(){
-    // Cabin列を取得する
-    const cabin_list = SHEET.getRange("K2:K").getValues();
-    
-    // 1次配列にする
-    const cabin_list_flat = cabin_list.flat();
+    // Cabin列を取得し、flatで1元配列化する
+    const cabin_list = SHEET.getRange("K2:K").getValues().flat();
     
     // 空白以外をフィルタする
-    const result = cabin_list_flat.filter(value => value != "");
+    const result = cabin_list.filter(value => value != "");
     
     // 結果
     console.log(result);
